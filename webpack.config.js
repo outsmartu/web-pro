4c78d2489012641d66f37293968d1c8032f7ec4a
artembell <artembell305@gmail.com>
2017-08-28 13:06:29 +0300

    Added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-  	app: './src/js/index.js',
-  },
-  plugins: [
-  	new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        }
-      ]
-    }
+	entry: {
+		app: './src/js/index.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		port: 8000
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}]
+	}
 };
