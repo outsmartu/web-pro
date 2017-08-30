638b810ffff4afc84fccb05cc46bb9f10eb5d6f2
maksimbell <maksimbell503@gmail.com>
2017-08-30 21:33:25 +0300

    Added Footer;
    Deleted test and tester.

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Link, NavLink, Switch} from 'react-router-dom';
 import Test from "./test";
 import Tester from "./tester";
-
+import Footer from "./components/Maksim/Footer";
 export default class App extends React.Component{
 	constructor(props) {
 		super(props);
@@ -15,7 +15,7 @@ export default class App extends React.Component{
 
 	render(){
 		return (
-			<Test/>
+			<Footer/>
 		);
 	}
 }
