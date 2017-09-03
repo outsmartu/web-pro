63288390afdd2801b81d02294ff44af5c03858f7
artembell <artembell305@gmail.com>
2017-09-03 18:52:02 +0300

    Little fixes

--- src/js/components/Artem/About.jsx
+++ src/js/components/Artem/About.jsx
@@ -1,37 +1,29 @@
 import React from "react";
 import {connect} from "react-redux";
 
-
 class About extends React.Component{
 	constructor(props) {
 		super(props);
 		    console.log("About", this.props.tracks);
 		
 		this.state = {
-			trackName: ''
+			
 		};
 	}
 
 	onTrackChange(e){
 		var track = e.target.value;
-		//console.log(track);
 		this.setState({
 			track: track
 		});
 	}
 
 	addTrack(){
-		console.log("added track", this.state.track);
 		this.props.onAddTrack(this.state.track);
 	}
 
-
 	render(){
-
-		//console.log(this.props.tracks);
-
 		return (
-
 			<div>
 				<h1>About music!</h1>
 				<input type="text" onChange={e => {this.onTrackChange(e)}}/>
